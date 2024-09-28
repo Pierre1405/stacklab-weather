55a77a1e21a62037c4642166b6798635aed98a69
Pierre1405 <pierre.corbel@gmail.com>
2024-09-28 09:56:29 +0200

    Add gitflow plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,20 @@
+import io.github.robwin.jgitflow.tasks.InitJGitflowTask
+
 plugins {
     id("net.researchgate.release") version "3.0.2"
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("io.github.robwin:jgitflow-gradle-plugin:0.6.0")
+    }
+}
+
+apply(plugin= "io.github.robwin.jgitflow")
+tasks.named(name = "initJGitflow", type = InitJGitflowTask::class) {
+    master = "main"
 }
