d363706afefb4428ec36f6924ea78553f0a093dc
Pierre1405 <pierre.corbel@gmail.com>
2024-10-07 12:20:16 +0200

    Create evaluation scenario test in WeatherbitWeatherServiceTest

--- weather/src/main/kotlin/com/stacklabs/weather/service/WeatherbitWeatherService.kt
+++ weather/src/main/kotlin/com/stacklabs/weather/service/WeatherbitWeatherService.kt
@@ -26,44 +26,58 @@ class WeatherbitWeatherService @Autowired constructor(
             )
         }
 
+    private fun throwIfFieldNull(entity: WeatherForecastEntity) {
+        throwIfValueNull(entity.temperature, "temperature")
+        throwIfValueNull(entity.pressure, "pressure")
+        throwIfValueNull(entity.windSpeed, "windSpeed")
+    }
+
+    private fun throwIfValueNull(value: Double?, fieldName: String) =
+        value ?: throw WeatherServiceException("Not able to retrieve weather forecast, a $fieldName is null")
+
     override fun getWeatherForecast(city: String): WeatherForecastDto {
         val weatherForecasts = repository.getWeatherForecastByCity(city).data
-        val referenceDay = weatherForecasts.minBy { it.datetime }
+        weatherForecasts.forEach(::throwIfFieldNull)
 
-        val sum = weatherForecasts.fold(WeatherForecastEntity(0.0, 0.0, 0.0)) { acc, forecast ->
-            WeatherForecastEntity(
-                temperature = acc.temperature!! + (forecast.temperature
-                    ?: throw WeatherServiceException("Not able to retrieve weather forecast, a temperature is null")),
-                pressure = acc.pressure!! + (forecast.pressure
-                    ?: throw WeatherServiceException("Not able to retrieve weather forecast, a pressure is null")),
-                windSpeed = acc.windSpeed!! + (forecast.windSpeed
-                    ?: throw WeatherServiceException("Not able to retrieve weather forecast, a wind speed is null"))
-            )
-        }
+        val today: WeatherForecastEntity = weatherForecasts.minBy { it.datetime }
+
+        val otherDays = weatherForecasts
+            // we must remove the reference day in order to avoid error on big tendency
+            .filterNot { it == today }
+        val otherDaysSum = otherDays
+            .fold(WeatherForecastEntity(0.0, 0.0, 0.0)) { acc, forecast ->
+                WeatherForecastEntity(
+                    temperature = acc.temperature!! + forecast.temperature!!,
+                    pressure = acc.pressure!! + forecast.pressure!!,
+                    windSpeed = acc.windSpeed!! + forecast.windSpeed!!,
+                )
+            }
 
         // lot of !!, but, it should never happen due to the exceptions throws on null values in the previous fold
-        val nbDays = weatherForecasts.size
-        val daysAverage = WeatherForecastEntity(
-            temperature = sum.temperature!! / nbDays,
-            pressure = sum.pressure!! / nbDays,
-            windSpeed = sum.windSpeed!! / nbDays
+        val nbOtherDays = otherDays.size
+        val otherDaysAverage = WeatherForecastEntity(
+            temperature = otherDaysSum.temperature!! / nbOtherDays,
+            pressure = otherDaysSum.pressure!! / nbOtherDays,
+            windSpeed = otherDaysSum.windSpeed!! / nbOtherDays
         )
 
         return WeatherForecastDto(
             globalTendency = Tendency.get(
-                forecastEvaluation.evaluate(referenceDay),
-                forecastEvaluation.evaluate(daysAverage)
+                forecastEvaluation.evaluate(today),
+                forecastEvaluation.evaluate(otherDaysAverage)
             ),
             temperatureTendency = Tendency.get(
-                referenceDay.temperature!!,
-                daysAverage.temperature!!
+                today.temperature!!,
+                otherDaysAverage.temperature!!
             ),
             pressureTendency = BigTendency.get(
-                referenceDay.pressure!!,
-                daysAverage.pressure!!,
+                today.pressure!!,
+                otherDaysAverage.pressure!!,
+                // Check big pressure fall on 7 days doesn't really make sense
+                // Normally we should check if the pressure fall during the last 4 hours
                 pressureBigFallDelta
             ),
-            windAverage = BeaufortScale.getFromMeterPerSeconds(daysAverage.windSpeed!!)
+            windAverage = BeaufortScale.getFromMeterPerSeconds(otherDaysAverage.windSpeed!!)
         )
     }
 
