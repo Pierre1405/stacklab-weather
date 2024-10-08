6fe7b43c6e452a15520124f45fb1e31077621834
Pierre1405 <pierre.corbel@gmail.com>
2024-10-08 12:04:58 +0200

    Use sealed class to manage success and error

--- weather/src/main/kotlin/com/stacklabs/weather/repository/WeatherBitRepositoryException.kt
+++
@@ -1,8 +0,0 @@
-package com.stacklabs.weather.repository
-
-open class WeatherBitRepositoryException(message: String, override val cause: Throwable? = null) :
-    RuntimeException(message, cause)
-
-class CityNotFoundWeatherBitRepositoryException(city: String) :
-    WeatherBitRepositoryException(message = "City $city not found")
-
