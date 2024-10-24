7e8137486f6d08e95da9565d3ee504671f4609d1
Pierre1405 <pierre.corbel@gmail.com>
2024-10-24 12:37:46 +0200

    Add logs to cache

--- weather/src/main/kotlin/com/stacklabs/weather/configuration/AppConfiguration.kt
+++ weather/src/main/kotlin/com/stacklabs/weather/configuration/AppConfiguration.kt
@@ -1,51 +1,44 @@
 package com.stacklabs.weather.configuration
 
-import com.github.benmanes.caffeine.cache.Cache
 import com.github.benmanes.caffeine.cache.Caffeine
-import com.stacklabs.weather.cache.Result
+import com.stacklabs.weather.cache.DelegateCacheWithLog
 import com.stacklabs.weather.cache.WeatherBitCachePolicy
 import com.stacklabs.weather.repository.WeatherBitRepository
 import com.stacklabs.weather.service.evaluation.TemperatureAndPressureForecastEvaluation
 import com.stacklabs.weather.weatherbit.models.CurrentObsGroup
 import com.stacklabs.weather.weatherbit.models.ForecastDay
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestClient
-import kotlin.time.DurationUnit
-import kotlin.time.toDuration
 
 
 @Configuration
 class AppConfiguration {
 
+    @Value("\${external.weatherbit.cache-size}") lateinit var cacheSize: String
+
+
     @Bean
     fun getWeatherBitClient(
-        weatherBitProperties: WeatherBitProperties,
-        currentApiCache: Cache<String, Result<CurrentObsGroup>>
-    ) =
-        WeatherBitRepository(
-            configuration = weatherBitProperties,
-            restClient = RestClient.create(weatherBitProperties.baseUrl),
-            currentApiCache = currentApiCache,
-            forecastCache = Caffeine.newBuilder().expireAfter(
-                WeatherBitCachePolicy<ForecastDay>()
-            ).build()
+        weatherBitProperties: WeatherBitProperties
+    ) = WeatherBitRepository(
+        configuration = weatherBitProperties,
+        restClient = RestClient.create(weatherBitProperties.baseUrl),
+        currentApiCache = DelegateCacheWithLog(
+            delegate = Caffeine.newBuilder()
+                .maximumSize(cacheSize.toLong())
+                .expireAfter(WeatherBitCachePolicy<CurrentObsGroup>())
+                .build(),
+            name = "Current"
+        ),
+        forecastCache = DelegateCacheWithLog(
+            delegate = Caffeine.newBuilder()
+                .maximumSize(cacheSize.toLong())
+                .expireAfter(WeatherBitCachePolicy<ForecastDay>()).build(),
+            name = "Forecast"
         )
-
-    @Bean
-    fun currentApiCache(weatherBitProperties: WeatherBitProperties): Cache<String, Result<CurrentObsGroup>> =
-        Caffeine.newBuilder()
-            .expireAfter(
-                WeatherBitCachePolicy<CurrentObsGroup>(
-                    maxCacheLifeDuration = weatherBitProperties
-                        .currentWeatherRefreshCacheDurationInMinutes
-                        .toDuration(DurationUnit.MINUTES),
-                )
-            ).build()
-
-    @Bean
-    fun forecastApiCache(weatherBitProperties: WeatherBitProperties): Cache<String, Result<ForecastDay>> =
-        Caffeine.newBuilder().expireAfter(WeatherBitCachePolicy<ForecastDay>()).build()
+    )
 
     @Bean
     fun weatherEvaluation(weatherEvaluationConfiguration: ForecastEvaluationConfiguration) =
